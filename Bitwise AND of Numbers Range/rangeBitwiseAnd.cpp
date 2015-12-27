a083d20869578c5500a9e0508b158aabf3585fd0
Lu Ma <somnusfish@gmail.com>
2015-12-26 23:01:20 -0500

    201

---
+++ Bitwise AND of Numbers Range/rangeBitwiseAnd.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int i = 0;
+        while(m!=n){
+            i++;
+            m >>= 1;
+            n >>= 1;
+        }
+        return m<<i;
+    }
+};
