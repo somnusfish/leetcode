a958a22a78612d82496404f83c366b238e007b20
Lu Ma <somnusfish@gmail.com>
2016-04-26 21:59:02 -0400

    update two sum

--- Two Sum/twoSum.cpp
+++ Two Sum/twoSum.cpp
@@ -3,21 +3,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ret;
-        multimap<int, int> numsMap;
-        multimap<int, int>::const_iterator c_it;
+        map<int, int> numsMap;
+        map<int, int>::const_iterator c_it;
         for(int i=0; i<nums.size(); i++){
-            numsMap.insert(make_pair(nums[i], i));
+            numsMap[nums[i]] = i;
         }
         
         for(int i=0; i<nums.size(); i++){
-            c_it = numsMap.find(nums[i]);
-            numsMap.erase(c_it);
-            int temp = target - nums[i];
-            c_it = numsMap.find(temp);
-            if(c_it!=numsMap.cend()){
+            c_it = numsMap.find(target-nums[i]);
+            if((c_it!=numsMap.cend()) && (c_it->second!=i)){
                 ret.push_back(i);
-                ret.push_back((*c_it).second);
-                
+                ret.push_back(c_it->second);
+                break;
             }
         }
         
